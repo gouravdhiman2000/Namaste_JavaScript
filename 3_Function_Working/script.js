f63b35b786783d7838740f4e9be25e89476ac80a
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-07 19:42:43 +0530

    undefined vs not-defined in JS

--- 3_Function_Working/script.js
+++ 3_Function_Working/script.js
@@ -1,13 +1,24 @@
-var x = 1;
-a();
-b();
-console.log(x);
-function a(){
-    var x = 10;
-    console.log(x);
-}
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+// function a(){
+//     var x = 10;
+//     console.log(x);
+// }
 
-function b(){
-    var x = 100;
-    console.log(x);
+// function b(){
+//     var x = 100;
+//     console.log(x);
+// }
+
+console.log(a);
+var a = 7;
+// console.log(x);
+console.log(a);
+if(a === undefined){
+    console.log("a is undefined");
+}
+else{
+    console.log("a is  not undefined");
 }
