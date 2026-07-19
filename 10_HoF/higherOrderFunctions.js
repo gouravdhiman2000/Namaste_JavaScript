4a87d9c20ba1b6fdc1afd7b832770aaef6ab350b
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-19 13:53:39 +0530

    Higher Order Function, Functional Programming

--- 10_HoF/higherOrderFunctions.js
+++ 10_HoF/higherOrderFunctions.js
@@ -9,35 +9,65 @@ function y(x){ //y-> HOF , X-> is callback function
 
 y(x);
 
+// const radius = [3,1,2,4];
+// //Task to calculate Area of given radius array
+// const calculateArea = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(Math.PI * radius[i] * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculateArea(radius));
+
+// //Task to calculate circumference of given radius array
+// const calculatecircumference = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(2 * Math.PI * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculatecircumference(radius));
+
+// //Task to calculate diameter of given radius array
+// const calculatediameter = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(2* radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculatediameter(radius));
+
+
 const radius = [3,1,2,4];
-//Task to calculate Area of given radius array
-const calculateArea = function(radius){
-    const output = [];
-    for(let i=0; i < radius.length; i++){
-        output.push(Math.PI * radius[i] * radius[i]);
-    }
-    return output;
-};
-console.log(calculateArea(radius));
 
-//Task to calculate circumference of given radius array
-const calculatecircumference = function(radius){
-    const output = [];
-    for(let i=0; i < radius.length; i++){
-        output.push(2 * Math.PI * radius[i]);
-    }
-    return output;
+const area = function(radius){
+    return Math.PI * radius * radius;
 };
-console.log(calculatecircumference(radius));
 
-//Task to calculate diameter of given radius array
-const calculatediameter = function(radius){
+const circumference = function(radius){
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function(radius){
+    return 2 * radius;
+};
+
+//Calculate function is Hof and area is callback function
+//using the same calculate function for area, circumference and diameter logic
+const calculate = function(radius, logic){
     const output = [];
-    for(let i=0; i < radius.length; i++){
-        output.push(2* radius[i]);
+    for(let i=0; i<radius.length; i++){
+        output.push(logic(radius[i]));
     }
     return output;
 };
-console.log(calculatediameter(radius));
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
+
+
 
 
---
+++ 10_HoF/tempCodeRunnerFile.js
@@ -0,0 +1,30 @@
+const radius = [3,1,2,4];
+// //Task to calculate Area of given radius array
+// const calculateArea = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(Math.PI * radius[i] * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculateArea(radius));
+
+// //Task to calculate circumference of given radius array
+// const calculatecircumference = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(2 * Math.PI * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculatecircumference(radius));
+
+// //Task to calculate diameter of given radius array
+// const calculatediameter = function(radius){
+//     const output = [];
+//     for(let i=0; i < radius.length; i++){
+//         output.push(2* radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculatediameter(radius));
