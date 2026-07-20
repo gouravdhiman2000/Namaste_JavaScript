2ef18d78be17d8ebc6a60643a4cc2c289fe98bb5
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-21 02:23:01 +0530

    Creating a first server using Node JS

--- 14_NodeJS/index.js
+++ 14_NodeJS/index.js
@@ -1,31 +1,19 @@
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join(__dirname, "data.json");
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    if(req.url === '/' && req.method === 'GET'){
+        res.writeHead(200, {"Content-Type" : "text/plain"});
+        res.end("Welcome to the Job Tracker API")
+    }
+    else if(req.url === '/health' && req.method === 'GET'){
+        res.writeHead(200, {"Content-Type" : "application/json"});
+        res.end(JSON.stringify({status : "ok"}));
+    }
+    else{
+        res.writeHead(404, {"Content-Type" : "text/plain"});
+        res.end("Not Found");
+    }
+});
 
-//Sync (Simple , blocks the thread)
-// const data = fs.readFileSync(filePath, "utf-8");
-// //console.log(data);
-
-// //Async (preferred in real apps)
-// fs.readFile(filePath, "utf-8", (err,data)=>{
-//     // if(err) throw err;
-//     // console.log(JSON.parse(data));
-// })
-
-const newData = {
-    applications: [
-        {
-          "id": 1,
-          "company": "Google",
-          "role": "SWE",
-          "status": "applied",
-          "appliedDate": "2026-06-01"
-        }
-    ], 
-};
-
-//for write
-fs.writeFile(filePath, JSON.stringify(newData, null, 2), (err)=>{
-    if(err) throw err;
-    console.log("Async write complete");
-})
+server.listen(3000,()=>{
+    console.log("Server running on http://localhost:3000");
+});
