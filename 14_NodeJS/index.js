010d62d1808e36ef145969ff4d24e94340b91ac7
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-21 02:45:12 +0530

    Middlewares and error handling

--- 14_NodeJS/index.js
+++ 14_NodeJS/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express()
 
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+
+
 app.get("/", (req,res) =>{
     res.send("Welcome to the Job tracker API");
 });
@@ -13,5 +22,12 @@ app.get("/application", (req,res) =>{
     const {status} = req.query;
     res.send(`Filtering by status : ${status}`);
 });
+
+//handling errors using middlewares
+app.use((err,req,res, next)=>{
+    console.log(err.stack);
+    res.status(500).json({error: "Something went Wrong"})
+});
+
 
 app.listen(3000, ()=>console.log("Server running on port : 3000"));
